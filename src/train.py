748b072639648933e699cc888f33dcbdc4568347
“Raphael <raphaellu.sem@gmail.com>
2026-04-12 23:31:44 -0400

    EOD push 04_12_26

--- src/train.py
+++ src/train.py
@@ -6,7 +6,6 @@ import sys
 import pickle
 import csv
 import yaml
-
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
